f65397a0c1ff854155ea61ad0ed766dbf6825fd6
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-15 21:41:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let button = document.getElementById("enterBtn");
 let text = document.getElementById("status");
  function remove(){
 	text.remove();
+	 let heading = document.createElement("h1");
+	 heading.innerText ="Entered Metaverse";
+	 document.body.appendChild(heading);
  }
 button.addEventListener("click",remove);
