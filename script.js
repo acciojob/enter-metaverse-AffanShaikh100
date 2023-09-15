6a508b947a6fed3178bb9caa0563eb154adc85d0
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-15 20:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let button = document.getElementById("enterBtn");
+let text = document.getElementById("status");
+ function remove(){
+	text.remove();
+ }
+button.addEventListener("click",remove);
