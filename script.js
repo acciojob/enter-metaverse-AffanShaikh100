6cedc7230eec84d9587636b0465bdbe731dbbc93
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-15 22:10:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 let button = document.getElementById("enterBtn");
 let text = document.getElementById("status");
  function remove(){
-	text.remove();
-	 let heading = document.createElement("h1");
-	 heading.innerText ="Entered Metaverse";
-	 document.body.insertBefore(heading, button);
+	// text.remove();
+	//  let heading = document.createElement("h1");
+	//  heading.innerText ="Entered Metaverse";
+	//  document.body.insertBefore(heading, button);
+	 text.innerHTML="<h1>Entered Metaverse</h1>"
  }
 button.addEventListener("click",remove);
